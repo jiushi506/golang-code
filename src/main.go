12ee051e9eb943ec20cf0f0f3fb6926bd632f650
astonguo <guoyh@rd.netease.com>
2018-10-07 21:12:11 +0800

    commit code

--- src/main.go
+++ src/main.go
@@ -11,6 +11,8 @@ import (
 	"io"
 	"reflect"
 	"unsafe"
+	"github.com/gin-gonic/gin"
+	"github.com/DeanThompson/ginpprof"
 )
 
 /**
@@ -33,7 +35,26 @@ func main() {
 	//wait_group_test()
 	//unsafe_pointer_uintptr_test()
 	//var_test()
-	map_test()
+	//map_test()
+	ginpprof_test();
+}
+
+func ginpprof_test() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	// automatically add routers for net/http/pprof
+	// e.g. /debug/pprof, /debug/pprof/heap, etc.
+	ginpprof.Wrap(router)
+
+	// ginpprof also plays well with *gin.RouterGroup
+	// group := router.Group("/debug/pprof")
+	// ginpprof.WrapGroup(group)
+
+	router.Run(":8080")
 }
 
 func map_test() {
